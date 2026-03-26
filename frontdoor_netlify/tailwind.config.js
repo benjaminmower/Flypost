6e2e4377b363acf486c5e7b8d67628b9cffac7c0
Flypost <bronco@goflypost.com>
2026-03-26 09:16:26 -0700

    fix: add air_force_blue shades, fix grids with inline CSS

--- frontdoor_netlify/tailwind.config.js
+++ frontdoor_netlify/tailwind.config.js
@@ -8,7 +8,12 @@ module.exports = {
         hot_berry: '#d82e7e',
         mint_leaf: '#40c9a2',
         bright_snow: '#f7f7f7',
-        air_force_blue: '#628395',
+        air_force_blue: {
+          DEFAULT: '#628395',
+          700: '#4a6270',
+          800: '#3d505c',
+          900: '#2f3e47',
+        },
         lemon_lime: '#e0e03e'
       }
     }
